b78ee12a45e9d9c46df6ec2769c12eda9a5c757f
Ivan Bochkarev <ibochkarev@uma.tech>
2024-04-09 10:52:43 +0600

    feat(search) Connecting Algolia search

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -726,9 +726,6 @@ export default defineConfigWithTheme<ThemeConfig>({
       indexName: 'ru-vuejs',
       appId: '5RTQ6TI35H',
       apiKey: 'c4095d6d1e93c25ca93b3ca0030338ef',
-      searchParameters: {
-        facetFilters: ['version:v3']
-      }
     },
 
     // carbonAds: {
